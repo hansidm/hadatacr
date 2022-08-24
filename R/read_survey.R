523beab2ddb3afc8bd714cee625d1167ab24d10c
Henrique Santos <hensantos@yahoo.com>
2022-08-24 15:32:33 -0400

    .

--- R/read_survey.R
+++ R/read_survey.R
@@ -3,6 +3,6 @@ read_survey <- function(file) {
     df_data <- read.csv(unz(file, 'data.csv'), header = T)
     df_weights <- read.csv(unz(file, 'weights.csv'), header = T)
     df_codebook <- read.csv(unz(file, 'codebook.csv'), header = T)
-    df <- left_join(df_data, df_weights, by="Human")
+    df <- dplyr::left_join(df_data, df_weights, by="Human")
     df
 }
