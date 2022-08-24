6040567a8ada1ff80f7e6ea442cf4afb45fa2938
Henrique Santos <hensantos@yahoo.com>
2022-08-24 15:28:22 -0400

    .

--- R/read_survey.R
+++ R/read_survey.R
@@ -1,8 +1,8 @@
 #' @export
 read_survey <- function(file) {
-    df <- read.csv(file)
-    read.csv(unz(file, 'data.csv'), header = T)
-    read.csv(unz(file, 'weights.csv'), header = T)
-    read.csv(unz(file, 'codebook.csv'), header = T)
+    df_data <- read.csv(unz(file, 'data.csv'), header = T)
+    df_weights <- read.csv(unz(file, 'weights.csv'), header = T)
+    df_codebook <- read.csv(unz(file, 'codebook.csv'), header = T)
+    df <- left_join(df_data, df_weights, by="Human")
     df
 }
